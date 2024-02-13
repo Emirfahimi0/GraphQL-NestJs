a0ac1a837f173adceb9227af1b97e8e0f2f7a6b2
Emirfahimi0 <emirfahimi0@gmail.com>
2024-02-13 14:12:14 +0800

    update types

--- src/jobs/job.type.ts
+++ src/jobs/job.type.ts
@@ -4,10 +4,13 @@ import { Field, ID, ObjectType } from "@nestjs/graphql";
 export class JobType {
 	@Field(() => ID)
 	id: string;
+
 	@Field()
 	jobTitle: string;
+
 	@Field()
 	companyName: string;
+
 	@Field()
 	jobDescription: string;
 }
