c2c3aa07783a835cdb2d2c0430c7e946bba458cf
Emirfahimi0 <emirfahimi0@gmail.com>
2024-02-13 14:13:30 +0800

    update module and main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { GraphQLModule } from "@nestjs/graphql";
 import { JobsModule } from "./jobs/jobs.module";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { Jobs } from "./jobs/job.entity";
 
 @Module({
 	imports: [
+		TypeOrmModule.forRoot({
+			type: "mongodb",
+			url: "mongodb://localhost/JobsFinder",
+			synchronize: true,
+			useUnifiedTopology: true,
+			entities: [Jobs],
+		}),
 		GraphQLModule.forRoot<ApolloDriverConfig>({
 			driver: ApolloDriver,
 			autoSchemaFile: true,
 		}),
 		JobsModule,
 	],
-	// controllers: [],
-	// providers: [],
 })
 export class AppModule {}
