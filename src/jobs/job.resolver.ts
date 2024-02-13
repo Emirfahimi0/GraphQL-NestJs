c55fdddfc07cf6ff630eaefa4f0508aee5febd30
Emirfahimi0 <emirfahimi0@gmail.com>
2024-02-13 14:13:06 +0800

    update resolver

--- src/jobs/job.resolver.ts
+++ src/jobs/job.resolver.ts
@@ -1,15 +1,24 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { JobType } from "./job.type";
+import { JobsService } from "./job.service";
+import { CreateJobsInput } from "./job.input";
 
 @Resolver(() => JobType)
 export class JobResolver {
+	constructor(private jobService: JobsService) {}
+
 	@Query(() => JobType)
-	Jobs() {
-		return {
-			id: "sdiadjadjja",
-			companyName: "SEEK Asia",
-			jobDescription: "JSJFSJFJSFJSFJSJFJ",
-			jobTitle: "React Native Developer",
-		};
+	jobDetails(@Args("id") id: string) {
+		return this.jobService.getJobsDetails(id);
+	}
+
+	@Query(() => [JobType])
+	allJobDetails() {
+		return this.jobService.getAllJobs();
+	}
+
+	@Mutation(() => JobType)
+	createJobs(@Args("CreateJobsInput") createJobsInput: CreateJobsInput) {
+		return this.jobService.createJobs(createJobsInput);
 	}
 }
